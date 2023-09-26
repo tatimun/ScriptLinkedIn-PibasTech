d794c87c6bdb7714c461c35e5e74eeebf0364eb0
tatianamunoz1 <tatianasmunoz@gmail.com>
2023-09-26 14:54:32 -0300

    ADD: Working code

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# PibasTechLinked
+
+# PibasTechLinked (BETA)
 A script used for connecting automatically using Python and Selenium (along with google apis) with everyone on the community of PibasTech 
+
+
+
+ ## Requerimientos:
+
+ - [Python](https://www.python.org)
+ - [ChromeDriver](https://community.chocolatey.org/packages/chromedriver) (Yo lo utilice con Chocolatey) 
+  Tiene que coincider con la version de tu Chrome Browser si queres que funcione
+ - [Credenciales de Google API](https://console.cloud.google.com/apis/) (Spreadsheet y Drive, habra que generarlas y agregarlas al path de este directorio)
+
+ - Una cuenta de LinkedIn :D
+
+
+
+ ### Pasos:
+
+ 1. Instalar lo mencionado arriba
+ 2. Instalar requerimientos de python 
+ 3. Correr el primer script [PrimerScript](./gettingMails.py), esto devuelve un csv con todos los LinkedIns del Spreadsheet
+
+ 4. Correr el [SegundoSript](./partSelenium.py), esto recorre cada script y conecta con cada uno de los linkedin. Es necesario agregar el email y contraseña de la cuenta
+
+ CUIDADO, LinkedIn a veces pide verificar que no seas robot, tenes 15 segundos para pasar la verificacion jaja
+
+
--- partSelenium.py
+++ partSelenium.py
@@ -10,10 +10,10 @@ driver = webdriver.Chrome()
 driver.get("https://www.linkedin.com")
 
 username = driver.find_element(By.ID, 'session_key')
-username.send_keys('EMAIL')
+username.send_keys('INSERTE MAIL @gmail.com')
 
 password = driver.find_element(By.ID, 'session_password')
-password.send_keys('PASSWORD')
+password.send_keys('INSERTE CONTRASEÑA')
 
 login_button = driver.find_element(By.CSS_SELECTOR, '.sign-in-form__submit-btn--full-width')
 login_button.submit()
@@ -27,19 +27,26 @@ with open('archivo.csv', 'r') as file:
             print(link)
             driver.get(link)
             time.sleep(5)
-            mas_button = driver.find_element(By.ID, 'ember338')
+            # mas_button = driver.find_element(By.ID, 'ember338')
 
-            select = Select(mas_button)
+            # select = Select(mas_button)
 
-            select.select_by_visible_text("Conectar")
+            # select.select_by_visible_text("Conectar")
             
-            wait = WebDriverWait(driver, 10)
-            mas_button = wait.until(EC.visibility_of_element_located((By.ID, 'ember338')))
-            button = driver.find_element(By.ID,'ember342')
-            button.click()
+            element = driver.find_element("xpath", '//div[3]/div/div[2]/button')
 
-            #connect_button = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, "ember407")))
-            # Haz clic en el botón "Conectar"
-            #connect_button.click()
+            # Realizar acciones con el elemento encontrado
+            element.click()
+            time.sleep(10)
+            print("hasta aca")
+            element2 = driver.find_element("xpath", '//div[2]/div/div/ul/li[3]/div')
+            element2.click()
+
+            print("hasta aca2")
+            time.sleep(15)
+            element3 = driver.find_element("xpath", '//button[contains(., "Enviar")]')
+            element3.click()
+            # print("Se agrego a " + link)
+
 driver.quit()
     
